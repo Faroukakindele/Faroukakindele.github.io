f5fde2dd88da8e7d4f18384480f04a59ccbd2bb5
Faroukakindele <torchbeareroau@gmail.com>
2022-07-30 16:20:27 +0100

    newPorfolio

--- index.js
+++ index.js
@@ -1,17 +1,57 @@
-"use strict";
-const allSection = document.querySelectorAll(".projects-items-header");
-const footer = document.querySelector(".footer");
-const observationCallBack = function (entries, observer) {
+const hamburmenu = document.querySelector(".hamburger");
+const bar = document.querySelector(".bar");
+const mobile_menu = document.querySelector(".navlist ul");
+const nav = document.querySelector("#nav");
+const hero = document.querySelector("#hero");
+const Allsections = document.querySelectorAll("section");
+hamburmenu.addEventListener("click", (e) => {
+  hamburmenu.classList.toggle("active");
+  mobile_menu.classList.toggle("active");
+});
+const nav_link = document.querySelectorAll(".navlist a");
+
+nav_link.forEach((link) =>
+  link.addEventListener("click", (e) => {
+    mobile_menu.classList.remove("active");
+  })
+);
+
+// sticky header
+const navHeight = nav.getBoundingClientRect().height;
+const headerCallBack = (entries, observer) => {
+  const [entry] = entries;
+  if (entry.isIntersecting) {
+    nav.classList.remove("color");
+  } else {
+    nav.classList.add("color");
+  }
+};
+const headerOptions = {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
+};
+const headerObserver = new IntersectionObserver(
+  headerCallBack,
+  headerOptions
+).observe(hero);
+
+// smooth reveal
+
+const sectionCallback = (entries, observer) => {
   const [entry] = entries;
   if (!entry.isIntersecting) return;
-  entry.target.classList.add("move");
+  entry.target.classList.remove("reveal");
   observer.unobserve(entry.target);
 };
-const observerOptions = {
+const sectionOptions = {
   root: null,
   threshold: 0.15,
 };
-const observer = new IntersectionObserver(observationCallBack, observerOptions);
-allSection.forEach((x) => {
-  observer.observe(x);
+const sectionReveal = new IntersectionObserver(sectionCallback, sectionOptions);
+Allsections.forEach((section) => {
+  section.classList.add("reveal");
+  sectionReveal.observe(section);
 });
+
+// Adding Links
